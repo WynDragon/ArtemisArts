8e12aa26beb3ea5d33071d862e95dfe8cddb7225
WynDragon <meeperviolin@gmail.com>
2021-10-29 20:05:36 -0600

    adding base models and viewmodels for skeletoning out the code

---
+++ ArtemisArtsTestWebsite_Take2/ArtemisArtsTestWebsite_Take2/Data/Models/Draft.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArtemisArtsTestWebsite_Take2.Data.Models
+{
+    public class Draft : BaseModel
+    {
+        [Key]
+        public int DraftId { get; set; } //id of draft to be published
+
+        [Required]
+        public int AccountId { get; set; } //who added the draft
+
+        [DataType(DataType.Text)]
+        public string Name { get; set; } //draft name
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string DisplayTitle { get; set; } //name given to the page
+
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; } //author note, side story, etc
+
+        [DataType(DataType.MultilineText)]
+        public string? Data { get; set; } //what it contains
+    }
+}
---
+++ ArtemisArtsTestWebsite_Take2/ArtemisArtsTestWebsite_Take2/Shared/ViewModels/IndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using ArtemisArtsTestWebsite_Take2.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ArtemisArtsTestWebsite_Take2.Shared.ViewModels
+{
+    public class IndexViewModel : ViewModel
+    {
+
+    }
+}
