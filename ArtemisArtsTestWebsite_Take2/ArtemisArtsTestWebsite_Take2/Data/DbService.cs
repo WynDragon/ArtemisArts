1bc3fbe5657fb149978d25477afc7bd04b78485e
WynDragon <meeperviolin@gmail.com>
2021-10-30 01:43:35 -0600

    setups in data service, nuget packages

---
+++ ArtemisArtsTestWebsite_Take2/ArtemisArtsTestWebsite_Take2/Data/DbService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using ArtemisArtsTestWebsite_Take2.Data.Models;
+
+namespace ArtemisArtsTestWebsite_Take2.Data
+{
+    public class DbService
+    {
+        private readonly _configuration;
+
+        #region WebcomicPage Queries
+        public async Task<List<Image>> GetPagesListAsync()
+        {
+            using var db = new SqlConnection(_configuration.GetConnectionString());
+            db.Open();
+            var history = await db.ExecuteAsync("Query goes here");
+            return history;
+        }
+        #endregion
+
+    }
+}
---
+++ ArtemisArtsTestWebsite_Take2/ArtemisArtsTestWebsite_Take2/Data/IDbService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ArtemisArtsTestWebsite_Take2.Data.Models;
+
+namespace ArtemisArtsTestWebsite_Take2.Data
+{
+    public interface IDbService
+    {
+        #region Account
+        public Task<bool> AddAccountAsync();
+        //public Task<List<Account>> GetAccountListAsync();
+        //public Task<Account> GetAccountAsync(string usrId);
+        //public Task<Account> GetAccountAsync(int accId);
+        //public Task<Account> GetAccountByEmailAsync(string email);
+        //public Task<bool> UpdateAccountAsync(Account acc);
+        public Task RemoveAccountAsync(int accId);
+        #endregion
+
+
+    }
+}
